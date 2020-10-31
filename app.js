3800bd5198412efa5ae32ee7c5568a90089b825a
masterdrue <masterdrue@gmail.com>
2020-10-30 22:06:13 -0400

    JWT

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +11,21 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+
+const mongoose = require('mongoose');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
+
 
 var app = express();
 
@@ -26,61 +38,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-      
-
-      //const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      //const user = auth[0];
-      //const pass = auth[1];
-  } else {
-      return next();
-  }
-}
-
-
-app.use(auth);
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
-
-const mongoose = require('mongoose');
-
-const url = 'mongodb://localhost:27017/nucampsite';
-const connect = mongoose.connect(url, {
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-});
-
-connect.then(() => console.log('Connected correctly to server'), 
-    err => console.log(err)
-);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

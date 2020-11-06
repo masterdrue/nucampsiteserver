055a6e45627c04695b1da1b4bdf97a9806207529
Drue McCombs <masterdrue@gmail.com>
2020-11-06 17:47:34 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-//Add New Properties
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017nucampsite',
-    'facebook': {
-        clientId: '666543607379364',
-        clientSecret: '8372bc08295e463b4c6fb4c7bf839747'
-    }
-}

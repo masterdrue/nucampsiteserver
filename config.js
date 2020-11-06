278b98876e128bd314499ee82e9d74f63635d64e
masterdrue <masterdrue@gmail.com>
2020-11-06 17:47:02 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017nucampsite',
     'facebook': {
-        clientId: 'YOUR',
-        clientSecret: 'MOMMA'
+        clientId: '666543607379364',
+        clientSecret: '8372bc08295e463b4c6fb4c7bf839747'
     }
 }
